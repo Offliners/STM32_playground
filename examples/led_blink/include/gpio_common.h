1888c37a0d79e09494296bc0dbeb4b0c0f0912c8
Offliners <eric21904@gmail.com>
2024-09-14 08:01:13 +0800

    update led blink

--- examples/led_blink/include/gpio_common.h
+++ examples/led_blink/include/gpio_common.h
@@ -6,9 +6,40 @@
 #define GPIOA_OFFSET    (0x00000000)
 #define GPIOA           GET_ADDR(PERIPHERAL_AHB1_BASE + GPIOA_OFFSET)
 
-#define GPIO_OSPEED_2MHZ		0x0
-#define GPIO_OSPEED_25MHZ		0x1
-#define GPIO_OSPEED_50MHZ		0x2
-#define GPIO_OSPEED_100MHZ		0x3
+// GPIOx_MODER (0x00)
+#define GPIO_MODER_OFFSET           0x00
+#define INPUT_RESET_STATE           0x00
+#define GENERAL_PURPOSE_OUTPUT_MODE 0x01
+#define ALTERNATE_FUNCTION_MODE     0x10
+#define ANALOG_MODE                 0x11
+
+#define RESET_GPIOA_MODER_PATTERN   0xA8000000
+#define RESET_GPIOA_MODER()                 WRITE_REG_LWORD(GPIOA, GPIO_MODER_OFFSET, RESET_GPIOA_MODER_PATTERN)
+#define CLEAR_MODER(pin)                    (~((0x11) << (2 * (pin))))
+#define CLEAR_GPIOX_MODER(gpiox, pin)       WRITE_REG_LWORD(gpiox, GPIO_MODER_OFFSET, READ_REG_LWORD(gpiox, GPIO_MODER_OFFSET) & CLEAR_MODER(pin))
+#define SET_MODER(pin, moder)               ((moder) << (2 * (pin)))
+#define SET_GPIOX_MODER(gpiox, pin, moder)  WRITE_REG_LWORD(gpiox, GPIO_MODER_OFFSET, READ_REG_LWORD(gpiox, GPIO_MODER_OFFSET) | SET_MODER(pin, moder))
+
+// GPIOx_PUPDR (0x0C)
+#define GPIO_PUPDR_OFFSET           0x0C
+#define NO_PULL_UP_PULL_DOWN        0x00
+#define PULL_UP                     0x01
+#define PULL_DOWN                   0x10
+#define PUPDR_RESERVED              0x11
+
+#define RESET_GPIOA_PUDER_PATTERN   0x64000000
+#define RESET_GPIOA_PUDER()                 WRITE_REG_LWORD(GPIOA, GPIO_PUPDR_OFFSET, RESET_GPIOA_PUDER_PATTERN)
+#define CLEAR_PUPDR(pin)                    (~((0x11) << (2 * (pin))))
+#define CLEAR_GPIOX_PUPDR(gpiox, pin)       WRITE_REG_LWORD(gpiox, GPIO_PUPDR_OFFSET, READ_REG_LWORD(gpiox, GPIO_PUPDR_OFFSET) & CLEAR_PUPDR(pin))
+#define SET_PUPDR(pin, pupdr)               ((pupdr) << (2 * (pin)))
+#define SET_GPIOX_PUPDR(gpiox, pin, pupdr)  WRITE_REG_LWORD(gpiox, GPIO_PUPDR_OFFSET, READ_REG_LWORD(gpiox, GPIO_PUPDR_OFFSET) | SET_PUPDR(pin, pupdr))
+
+// GPIOx_ODR (0x14)
+#define GPIO_ODR_OFFSET           0x14
+
+// GPIO set/clear/toggle
+#define SET_GPIOX(gpiox, pin)               WRITE_REG_LWORD(gpiox, GPIO_ODR_OFFSET, READ_REG_LWORD(gpiox, GPIO_ODR_OFFSET) | (0x1 << (pin)))
+#define CLEAR_GPIOX(gpiox, pin)             WRITE_REG_LWORD(gpiox, GPIO_ODR_OFFSET, READ_REG_LWORD(gpiox, GPIO_ODR_OFFSET) & (~(0x1 << (pin))))
+#define TOGGLE_GPIOX(gpiox, pin)            WRITE_REG_LWORD(gpiox, GPIO_ODR_OFFSET, READ_REG_LWORD(gpiox, GPIO_ODR_OFFSET) ^ (0x1 << (pin)))
 
 #endif
--- examples/led_blink/src/main.c
+++ examples/led_blink/src/main.c
@@ -1,11 +1,11 @@
 #include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/gpio.h>
+#include "gpio_common.h"
 #include "timer_func.h"
 
 #if defined(NUCLEO_F446RE)
   #define RCC_LED_GPIO  (RCC_GPIOA)   // Reset and Clock Control (RCC) port
   #define GPIO_LED_PORT (GPIOA)       // General Purpose Input Output (GPIO) port
-  #define GPIO_LED_PIN  (GPIO5)       // General Purpose Input Output (GPIO) pin
+  #define PIN5          (5)           // General Purpose Input Output (GPIO) pin
 #else
   #error "Your STM32 board is not NUCLEO-F446RE!"
 #endif
@@ -15,19 +15,24 @@ int main(void)
   // Enable clock
   rcc_periph_clock_enable(RCC_LED_GPIO);
 
-  // Set LED pin to output push-pull
-  gpio_mode_setup(GPIO_LED_PORT, GPIO_MODE_OUTPUT, GPIO_OSPEED_2MHZ, GPIO_LED_PIN);
+  RESET_GPIOA_MODER();
+  RESET_GPIOA_PUDER();
+
+  // Initial GPIO
+  CLEAR_GPIOX_MODER(GPIO_LED_PORT, PIN5);
+  SET_GPIOX_MODER(GPIO_LED_PORT, PIN5, GENERAL_PURPOSE_OUTPUT_MODE);
+  CLEAR_GPIOX_PUPDR(GPIO_LED_PORT, PIN5);
+  SET_GPIOX_PUPDR(GPIO_LED_PORT, PIN5, NO_PULL_UP_PULL_DOWN);
 
   // Led starts blinking
   while(true)
   {
-    // Led on / off
-    gpio_toggle(GPIO_LED_PORT, GPIO_LED_PIN);
-    
+    // toggle led2 
+    TOGGLE_GPIOX(GPIO_LED_PORT, PIN5);
+
     // 2000000 / GPIO_OSPEED_2MHZ = 1Hz
-    timer_delay(2000000, GPIO_OSPEED_2MHZ);
+    timer_delay(2000000);
   }
 
   return 0;
 }
-
